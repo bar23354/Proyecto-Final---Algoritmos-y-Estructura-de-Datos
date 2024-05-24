b03a048cc439372504a8be6509facfa524c6e849
bar23354 <bar23354@uvg.edu.gt>
2024-05-24 16:57:12 -0600

    Nueva organización del codigo

---
+++ Perfil.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Perfil {
+    private String id;
+    private List<String> intereses;
+    
+    //ayuda con los getters in setters pls!
+}
---
+++ Usuario.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Usuario {
+    private String id;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private List<String> gustos;
+    private List<String> disgustos;
+
+    //ayuda con los getters in setters pls!
+}
